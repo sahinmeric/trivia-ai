5e079d75c86d8ffa794c71b6e36e2d6e5bc6466d
Sahin Meric <sahin.meric@medisprout.com>
2024-10-27 19:39:46 -0500

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { auth } from "./firebase";
 import { onAuthStateChanged, User } from "firebase/auth";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import GameLobby from "./components/GameLobby";
 
@@ -14,7 +15,15 @@ const App: React.FC = () => {
     return () => unsubscribe();
   }, []);
 
-  return <div>{user ? <GameLobby /> : <Login />}</div>;
+  return (
+    <Routes>
+      <Route path="/" element={user ? <Navigate to="/lobby" /> : <Login />} />
+      <Route
+        path="/lobby"
+        element={user ? <GameLobby /> : <Navigate to="/" />}
+      />
+    </Routes>
+  );
 };
 
 export default App;
